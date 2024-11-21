dfce8dabb42a2ccc2aa005a7ffe9a2b19ace9b33
Kamil Kalarus <kalkalax@gmail.com>
2024-11-21 15:06:13 +0100

    Work time measurement implemented

--- TimeHubDesktop/MainWindow.xaml.cs
+++ TimeHubDesktop/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace TimeHubDesktop
 {
@@ -16,9 +17,63 @@ namespace TimeHubDesktop
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private readonly WorkTimeTracker _workTimeTracker;
+        private readonly RefreshTimer _refreshTimer;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Tworzymy obiekt WorkTimeTracker
+            _workTimeTracker = new WorkTimeTracker();
+
+            // Inicjalizacja timer'a do aktualizacji czasu 
+            _refreshTimer = new RefreshTimer(_workTimeTracker, UpdateTimeDisplay);
+            _refreshTimer.Start();
+          
         }
+
+        // Metoda, która będzie wywoływana przez RefreshTimer do zaktualizowania UI
+        private void UpdateTimeDisplay(TimeSpan totalWorkTime)
+        {
+            TimeLabel.Content = totalWorkTime.ToString(@"hh\:mm\:ss");
+        }
+
+
+        private void StartStopButton_Click(object sender, RoutedEventArgs e)
+        {
+            // jeśli działa
+            if (_workTimeTracker.IsTracking)
+            {
+                ResetButton.IsEnabled = true;
+                _workTimeTracker.StopTracking();
+                StartStopButton.Content = "Start";
+                Debug.WriteLine("Czas stop");
+            }
+            // jeśli jeszcze nie działa
+            else
+            {
+                ResetButton.IsEnabled = false;
+                _workTimeTracker.StartTracking();
+                StartStopButton.Content = "Stop";
+                Debug.WriteLine("Czas start");
+            }
+
+        }
+
+        // Metoda, która obsługuje naciśnięcie przycisku resetu (zakończenia sesji)
+        private void ResetButton_Click(Object sender, RoutedEventArgs e)
+        {
+            if (!_workTimeTracker.IsTracking)
+            {
+                Debug.WriteLine($"Reset, czas: {_workTimeTracker.TotalWorkTime}");
+                _workTimeTracker.ResetTracking();
+            }
+        }
+
+
+
+
     }
 }
---
+++ TimeHubDesktop/RefreshTimer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace TimeHubDesktop
+{
+    public class RefreshTimer
+    {
+        private readonly DispatcherTimer _timer;
+        private readonly WorkTimeTracker _workTimeTracker;
+        private readonly Action<TimeSpan> _updateAction;
+
+        // Konstruktor, który przyjmuje WorkTimeTracker i akcję do aktualizacji UI
+        public RefreshTimer(WorkTimeTracker workTimeTracker, Action<TimeSpan> updateAction)
+        {
+            _workTimeTracker = workTimeTracker;
+            _updateAction = updateAction;
+
+            // Inicjalizacja timer'a do aktualizacji czasu 
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(100) // Co pół sekundy
+            };
+            _timer.Tick += Timer_Tick; // Subskrybujemy zdarzenie Tick
+        }
+
+        // Metoda wywoływana przy każdym ticku timera
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            // Wywołanie akcji do zaktualizowania UI z bieżącym czasem
+            _updateAction(_workTimeTracker.TotalWorkTime);
+        }
+
+        public void Start() 
+        {
+            _timer.Start();
+        }
+    }
+}
